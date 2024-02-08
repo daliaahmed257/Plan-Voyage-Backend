0c639de7e690f42da80ee4b52384473c264f8084
Dalia Ahmed <da257@njit.edu>
2024-02-07 21:16:36 -0500

    google auth added

--- routes/trips.js
+++ routes/trips.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 const express = require("express")
 const router = express.Router()
+const passport = require('passport')
 
 const tripsCtrl = require('../controllers/trips')
 
@@ -14,5 +15,34 @@ router.post("/", tripsCtrl.create)
 
 //TRIPS SHOW ROUTE
 router.get("/:id", tripsCtrl.show)
+
+// Google OAuth login route
+router.get('/auth/google', passport.authenticate(
+    // Which passport strategy is being used?
+    'google',
+    {
+      // Requesting the user's profile and email
+      scope: ['profile', 'email'],
+      // Optionally force pick account every time
+      // prompt: "select_account"
+    }
+  ));
+
+  // Google OAuth callback route
+  router.get('/oauth2callback', passport.authenticate(
+    'google',
+    {
+      successRedirect: '/mytrips',
+      failureRedirect: '/explore'
+    }
+  ));
+
+// OAuth logout route
+router.get('/logout', function(req, res){
+    req.logout(function() {
+      res.redirect('/mytrips');
+    });
+  });
+
 
 module.exports = router
--- server.js
+++ server.js
@@ -11,18 +11,21 @@ const express = require("express");
 //create application object
 const app = express();
 
-// initialize .env variables
-require("dotenv").config();
-
 // start the mongoose db connection
 require('./config/db.connection.js')
+
+require('./config/passport.js')
 
 // import places router
 const placesRouter = require('./routes/places.js')
 const tripsRouter = require('./routes/trips.js')
 
 const cors = require("cors")
-const morgan = require("cors")
+const morgan = require("morgan")
+
+var session = require('express-session')
+var passport = require('passport')
+
 
 
 //MIDDLEWARE
@@ -31,6 +34,20 @@ app.use(express.json()); // parse json bodies
 
 app.use(cors()); // to minimize cors errors, open access to all origins
 app.use(morgan("dev")); // logging for development
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function (req, res, next) {
+    res.locals.user = req.user;
+    next();
+  });
 
 app.use('/explore', placesRouter)
 app.use('/mytrips', tripsRouter)
